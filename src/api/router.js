d891a20d1535786d783f975f905c5b10bcab73df
Dan Reeves <heydanreeves@gmail.com>
2017-05-20 12:23:53 +0100

    Add a logger

--- src/api/router.js
+++ src/api/router.js
@@ -1,30 +1,32 @@
 const { Router } = require('express');
+const pino = require('pino');
 
 const router = Router();
+const log = pino();
 
 router.get('/:key', function getKey(req, res) {
     // Get or create data for key
-    console.log('get', req.params.key);
+    log.info('get', req.params.key);
 });
 
 router.put('/:key', function updateKey(req, res) {
     // Update the data for key
-    console.log('update', req.params.key);
+    log.info('update', req.params.key);
 });
 
 router.delete('/:key', function deleteKey(req, res) {
     // Delete the data for key
-    console.log('delete', req.params.key);
+    log.info('delete', req.params.key);
 });
 
 router.get('/allkeys', function getAllKeys(req, res) {
     // Get all the keys currently in the cache
-    console.log('get all keys');
+    log.info('get all keys');
 });
 
 router.delete('/allkeys', function deleteAllKeys(req, res) {
     // Purge all the keys from the cache
-    console.log('delete all keys');
+    log.info('delete all keys');
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const pino = require('pino');
 const apiRouter = require('./api/router');
 
 const app = express();
+const log = pino();
+
+const PORT = 3000;
 
 app.use('/api', apiRouter);
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(PORT, () => {
+    log.info(`Listening on port ${PORT}`);
+});
